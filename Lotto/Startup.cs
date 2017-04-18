09bf1aa27645b2fd60e3e94dbeab08f5f2e5f65a
doctoryellow <doctoryellow@users.noreply.github.com>
2017-04-18 18:54:42 +0100

    Configured dependency injection

--- Lotto/Startup.cs
+++ Lotto/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Lotto.Core;
 
 namespace Lotto
 {
@@ -29,6 +30,10 @@ namespace Lotto
         {
             // Add framework services.
             services.AddMvc();
+
+            // Configure dependency injection
+            services.AddTransient<IRandomNumberService, SimpleRandomNumberService>();
+            services.AddTransient<ILuckyNumberService, LuckyNumberService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
