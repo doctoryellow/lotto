79c013ef128f1c0ae642cc7860424cc774b74ada
doctoryellow <doctoryellow@users.noreply.github.com>
2017-04-18 18:52:17 +0100

    Added implementation for Index action

--- Lotto.Tests/HomeControllerTests.cs
+++ Lotto.Tests/HomeControllerTests.cs
@@ -3,16 +3,25 @@ using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using Lotto.ViewModels;
+using Lotto.Core;
+using Moq;
+using System.Linq;
 
 namespace Lotto.Tests
 {
     public class HomeControllerTests
     {
         [Fact]
-        public void Index_ShouldReturnViewWithViewModel()
+        public void Index_ShouldReturnView_WithViewModelAndSixNumbers()
         {
             // Arrange
-            var controller = new HomeController();
+            var luckyNumberGeneratorMock = new Mock<ILuckyNumberService>();
+
+            luckyNumberGeneratorMock
+                .Setup(m => m.DrawNumbers())
+                .Returns(Enumerable.Range(1, 49));
+            
+            var controller = new HomeController(luckyNumberGeneratorMock.Object);
 
 
             // Act
@@ -22,8 +31,8 @@ namespace Lotto.Tests
             var viewResult = Assert.IsType<ViewResult>(result);
             var model = Assert.IsAssignableFrom<HomeViewModel>(viewResult.Model);
 
-
-
+            Assert.Equal(6, model.Numbers.Count);
+            Assert.Equal(new[] { 1, 2, 3, 4, 5, 6 }, model.Numbers);
         }
     }
 }
--- Lotto/Controllers/HomeController.cs
+++ Lotto/Controllers/HomeController.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Lotto.ViewModels;
+using Lotto.Core;
+using System.Linq;
 
 namespace Lotto.Controllers
 {
     public class HomeController : Controller
     {
+        private ILuckyNumberService _luckyNumberService;
+
+        public HomeController(ILuckyNumberService luckyNumberService)
+        {
+            _luckyNumberService = luckyNumberService;
+        }
+
         public IActionResult Index()
         {
-            var viewModel = new HomeViewModel();
+            var numbers = _luckyNumberService
+                .DrawNumbers()
+                .Take(6)
+                .ToList();
+
+
+            var viewModel = new HomeViewModel()
+            {
+                Numbers = numbers
+            };
 
             return View(viewModel);
         }
